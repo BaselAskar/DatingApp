292de2c15adb147e776c08b3c3d406655ec651f4
BaselAskar <76973409+BaselAskar@users.noreply.github.com>
2021-08-01 22:43:11 +0200

    Create Token Services

---
+++ API/Services/TokenServices.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace API.Services
+{
+    public class TokenServices : ITokenServices
+    {
+        private SymmetricSecurityKey _key;
+        public TokenServices(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+        }
+        public string CreateToken(AppUser user)
+        {
+            //initial Claims for token
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId,user.UserName)
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(2),
+                SigningCredentials = new SigningCredentials(_key,SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var tokenHandlar = new JwtSecurityTokenHandler();
+
+            var token = tokenHandlar.CreateToken(tokenDescriptor);
+
+            return tokenHandlar.WriteToken(token);
+        }
+    }
+}
